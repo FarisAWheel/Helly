7a5e5e04c23f985d45cca93ee9e3fa6a2c2bdde0
farisAwheel <farishabdelrahem@gmail.com>
2025-05-05 00:28:56 -0400

    download sharp and begin to process image attatchments

--- src/events/messageCreate/01-detectTylist.ts
+++ src/events/messageCreate/01-detectTylist.ts
@@ -1,13 +1,30 @@
-import { Attachment, Client, Embed, Message } from "discord.js";
+import { Attachment, Client, Message } from "discord.js";
+import sharp, { Sharp } from "sharp";
+
+async function getAverageColor(
+    url: string,
+    height: Number,
+    width: Number
+) {
+    // sharp can not handle networking so this doesn't work for now
+    const data = await sharp(url)
+        .jpeg({
+        quality: 50
+        })
+        .raw()
+        .toBuffer();
+
+    console.log(data);
+}
 
 export default async function handleTylist(
     client: Client,
     message: Message
 ) {
+
+
     if(message.channelId === '1195529657240539296') {
         const messageAttatchements: Attachment[] = [];
-        message.attachments.forEach(attachment => messageAttatchements.push(attachment));
-
-        console.log(messageAttatchements);
+        message.attachments.forEach(attachment => getAverageColor(attachment.url, 10, 10));
     }
 }
